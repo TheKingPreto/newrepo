8c5c7de289f3c1931cba553babb771e254b2f61b
TheKingPreto <lucasoliveirasilva2008@gmail.com>
2025-09-19 21:41:08 -0300

    w03 learning activity

--- server.js
+++ server.js
@@ -7,9 +7,10 @@
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -23,21 +24,22 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 
-// index rout
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+// index route
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT 
+const host = process.env.HOST 
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`app listening on ${host}:${port}`)
 })
