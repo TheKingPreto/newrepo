8dabd540b8c527255ea7196579439580799a008f
TheKingPreto <lucasoliveirasilva2008@gmail.com>
2025-09-25 20:43:28 -0300

    w04 learning activities

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute");
+const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities")
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * View Engine and Templates
@@ -22,6 +26,31 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
 
 /* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Body parser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
@@ -29,8 +58,11 @@ app.use(static)
 // index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
 
-// Inventory routes
+// Inventory route
 app.use("/inv", inventoryRoute)
+
+// Account route
+app.use("/account", accountRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
